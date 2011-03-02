684b81434b140a0f782d7d028151d30714d650b7
Вадим <vterranz@gmail.com>
2011-03-02 12:39:07 +0300

    пишу основной класс

--- graphlib/basegraphitem.cpp
+++ graphlib/basegraphitem.cpp
@@ -1,5 +1,8 @@
 #include "basegraphitem.h"
 
-BaseGraphItem::BaseGraphItem()
+BaseGraphItem::BaseGraphItem(
+        QString m_id, QString m_rem, QString m_text, QString m_next,
+        int m_type, int m_send_or_recv)
 {
+
 }
--- graphlib/basegraphitem.h
+++ graphlib/basegraphitem.h
@@ -7,7 +7,10 @@
 class BaseGraphItem : public QGraphicsItem
 {
 public:
-    BaseGraphItem();
+    BaseGraphItem(
+            QString m_id = "", QString m_rem = "", QString m_text = "", QString m_next = "",
+            int m_type = 0, int m_send_or_recv = 0
+                 );
 
 private:
     QString m_id,m_rem,m_text,m_next;
