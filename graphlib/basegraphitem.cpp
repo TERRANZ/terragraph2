114b637c4ced407bc39eaaf8b9b2df22344c5aae
Вадим <vterranz@gmail.com>
2011-03-02 12:46:30 +0300

    пишу основной класс

--- graphlib/basegraphitem.cpp
+++ graphlib/basegraphitem.cpp
@@ -1,8 +1,43 @@
 #include "basegraphitem.h"
 
 BaseGraphItem::BaseGraphItem(
-        QString m_id, QString m_rem, QString m_text, QString m_next,
-        int m_type, int m_send_or_recv)
+        QString id, QString rem, QString text, QString next,
+        int type, int send_or_recv)
 {
+    setId(id);
+    setRem(rem);
+    setText(text);
+    setNext(next);
+    setType(type);
+    setSendOrRecv(send_or_recv);
+}
 
+void BaseGraphItem::setId(QString id)
+{
+    m_id = id;
+}
+
+void BaseGraphItem::setRem(QString rem)
+{
+    m_rem = rem;
+}
+
+void BaseGraphItem::setText(QString text)
+{
+    m_text = text;
+}
+
+void BaseGraphItem::setNext(QString next)
+{
+    m_next = next;
+}
+
+void BaseGraphItem::setType(int type)
+{
+    m_type = type;
+}
+
+void BaseGraphItem::setSendOrRecv(int send_or_recv)
+{
+    m_send_or_recv = send_or_recv;
 }
--- graphlib/basegraphitem.h
+++ graphlib/basegraphitem.h
@@ -8,10 +8,22 @@ class BaseGraphItem : public QGraphicsItem
 {
 public:
     BaseGraphItem(
-            QString m_id = "", QString m_rem = "", QString m_text = "", QString m_next = "",
-            int m_type = 0, int m_send_or_recv = 0
-                 );
+            QString id = "", QString rem = "", QString text = "", QString next = "",
+            int type = 0, int send_or_recv = 0
+                                                 );
 
+    QString id() {return m_id;};
+    QString rem() {return m_rem;};
+    QString text() {return m_text;};
+    QString next() {return m_next;};
+    int     type() {return m_type;};
+    int     send_or_recv() {return m_send_or_recv;};
+    void setId(QString id);
+    void setRem(QString rem);
+    void setText(QString text);
+    void setNext(QString next);
+    void setType(int type);
+    void setSendOrRecv(int send_or_recv);
 private:
     QString m_id,m_rem,m_text,m_next;
     int m_type,m_send_or_recv;
